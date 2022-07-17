1576391b28437476d89bf8fd90ef7063d2e84bb2
rakeshrgit <rakesh2abhishek@gmail.com>
2022-07-17 14:43:54 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class App extends Component {
+  state = {  } 
+  render() { 
+    return (
+      <main className="container">
+        <h1>Hello World</h1>
+      </main>
+    );
+  }
+}
+ 
+export default App;
