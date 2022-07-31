6103e7727cb839176f58706e6848a565ab92e144
rakeshrgit <rakesh2abhishek@gmail.com>
2022-07-31 15:05:16 +0530

    add all files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+const apiEndpoint="https://jsonplaceholder.typicode.com/posts"; 
 class App extends Component {
-  state = {  } 
+  state = { 
+    posts:[]
+   } 
+  
+  async componentDidMount() {
+    const {data:posts} = await axios.get('https://jsonplaceholder.typicode.com/posts');
+    this.setState({posts})
+    //console.log('response', response)
+  }
+  
+  // handleUpdate = (async post) =>{
+  //   post.title = "UPDATED";
+  //   const {data} = await axios.put(apiEndpoint + '/' + post.id, post); 
+  // }
+
+  handleDelete = async post => {
+    const posts = this.state.posts.filter(p => p.id !== post.id);
+    this.setState({posts});
+    
+    await axios.delete(apiEndpoint + '/' + post.id);
+
+    
+  }
   render() { 
     return (
       <main className="container">
-        <h1>Hello World</h1>
+         <table className="table">
+            <thead>
+              <tr>
+                <th>Title</th>
+                <th>Update</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+               {this.state.posts.map(post=>(
+                  <tr key={post.id}>
+                      <td>{post.body}</td>  
+                      <td>{post.title}</td>
+                      <td><button className="btn btn-danger btn-sm" onClick={() => this.handleDelete(post)}>Delete</button></td>
+                  </tr>
+               ))} 
+
+            </tbody>
+         </table>
       </main>
     );
   }
---
+++ src/services/httpService.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+axios.interceptors.response.use(null, error => {
+  const expectedError =
+    error.response &&
+    error.response.status >= 400 &&
+    error.response.status < 500;
+  
+  if (!expectedError) {
+    console.log("UnexpectedError =>", error);
+    
+  }
+//   if (expectedError) {
+//     console.log("UnexpectedError =>",error.response.data.message);
+//   }
+  return Promise.reject(error);
+});
+
+export default {
+  get: axios.get,
+  post: axios.post,
+  put: axios.put,
+  delete: axios.delete
+};
